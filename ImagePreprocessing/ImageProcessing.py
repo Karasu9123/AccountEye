fa668d5f8539d7126fbe45ef52f655e977758abe
Karasu <karasu9123@gmail.com>
2019-03-06 18:41:53 +0200

    Change preproc method.

--- ImagePreprocessing/ImageProcessing.py
+++ ImagePreprocessing/ImageProcessing.py
@@ -83,13 +83,14 @@ def NewAugmentation(loadCSVFilePath, CSVFileName, loadPicturesPath, savePath, re
     new_csv_file.to_csv(savePath + '/' + CSVFileName[:-4] + '_Blur+Augmentation.csv')
     cv2.waitKey(0)
 
-"""
-def HOG():
-    hog = cv2.HOGDescriptor(_winSize=(32,48), _blockSize=(4,4), _blockStride=(2,2), _cellSize=(2,2), _nbins=9, _gammaCorrection=True)
-    magnitudes, angles = hog.computeGradient(image)
-    sobel = cv2.imshow("digit %s" % i, np.sqrt(np.power(magnitudes[..., 0],2) + np.power(magnitudes[..., 1],2)))
-    return sobel 
-"""
+
+def hog(im, ksize = 1):
+    im = np.float32(im)
+    gx = cv2.Sobel(im, cv2.CV_32F, 1, 0, ksize=ksize)
+    gy = cv2.Sobel(im, cv2.CV_32F, 0, 1, ksize=ksize)
+    grad = np.uint8(np.sqrt(np.power(gx, 2) + np.power(gy, 2)))
+    mag, angle = cv2.cartToPolar(gx, gy, angleInDegrees=True)
+    return grad
 
 def sobel(img, ksize=1):
     img = np.float32(img)
@@ -97,13 +98,16 @@ def sobel(img, ksize=1):
     gy = cv2.Sobel(img, cv2.CV_32F, 0, 1, ksize=ksize)
     return np.uint8(np.sqrt(np.power(gx, 2) + np.power(gy, 2)))
 
-def clahe(img):
-    lab = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
-    l, a, b = cv2.split(lab)
-    clahe = cv2.createCLAHE(clipLimit=5.0, tileGridSize=(3, 3))
-    cl = clahe.apply(l)
-    limg = cv2.merge((cl, a, b))
-    img = cv2.cvtColor(limg, cv2.COLOR_LAB2BGR)
+def clahe(img, clipLimit=5.0, tileGridSize=(3, 3)):
+    clahe = cv2.createCLAHE(clipLimit, tileGridSize)
+    if img.ndim == 3:
+        lab = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
+        l, a, b = cv2.split(lab)
+        cl = clahe.apply(l)
+        limg = cv2.merge((cl, a, b))
+        img = cv2.cvtColor(limg, cv2.COLOR_LAB2BGR)
+    else:
+        img = clahe.apply(img)
     return img
 
 def dog(im, inner, outer):
@@ -111,39 +115,52 @@ def dog(im, inner, outer):
     outerBlur = cv2.GaussianBlur(im, (0, 0), outer).astype(np.int)
     return np.clip(innerBlur - outerBlur, 0, 255).astype(np.ubyte)
 
+def equalize(im):
+    if im.ndim == 3:
+        lab = cv2.cvtColor(im, cv2.COLOR_BGR2LAB)
+        l, a, b = cv2.split(lab)
+        l = cv2.equalizeHist(l)
+        lab = cv2.merge((l, a, b))
+        im = cv2.cvtColor(lab, cv2.COLOR_LAB2BGR)
+    else:
+        im = cv2.equalizeHist(im)
+    return im
+
 def sharpen(im, sigma, amount):
     blur = cv2.GaussianBlur(im, (0, 0), sigma)
-    im = amount * (im.astype(np.int) - blur.astype(np.int))
+    im = (amount + 1) *  im.astype(np.int) - amount * blur.astype(np.int)
     return np.clip(im, 0, 255).astype(np.ubyte)
 
 def preproc(im):
-    gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
-    #if np.mean(gray) > 127:
-    #    im = 255 - gray
-    #    im = cv2.cvtColor(im, cv2.COLOR_GRAY2BGR)
-    im = sharpen(im, 10, 4)
-    cv2.threshold(im, np.median(im) + np.mean(im) / 2, 255, cv2.THRESH_TOZERO, im)
-    im = dog(im, 1, 4)
-    cv2.threshold(im, np.median(im) + np.mean(im) / 2, 255, cv2.THRESH_TOZERO, im)
-    im = clahe(im)
-    im = cv2.GaussianBlur(im, (0, 0), 0.5)
+    cv2.erode(im, (3, 3), im, iterations=1)
+    im = cv2.bilateralFilter(im, 3, 10, 10)
+    im = sharpen(im, 7, 2)
+    im = cv2.bilateralFilter(im, 3, 3, 3)
+    im = sharpen(im, 2, 1)
+    mask = (im[:, :, 2] > cv2.add(im[:, :, 0], im[:, :, 1]))
+    im[mask] = 0
     im[:, :, 2] = 0
+    im = dog(im, 1, 6)
     im = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
-    #im = cv2.resize(im, (32, 48))
+    cv2.threshold(im, 0, 255, cv2.THRESH_TOZERO + cv2.THRESH_OTSU, im)
+    im = sharpen(im, 10, 1)
+    im = sobel(im)
+    cv2.threshold(im, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU, im)
     return im
 
 def main():
-    NewAugmentation("/home/shared/AccountEye/Images/Labels",
+    """NewAugmentation("/home/shared/AccountEye/Images/Labels",
                     "All.csv",
                     "/home/shared/AccountEye/Images/BlurAndThreshold",
-                    "/home/shared/AccountEye/Images/Augmented/BlurAll")
-    """sets = ["Meter_2/", "Meter_1/", "YouTube/"]
+                    "/home/shared/AccountEye/Images/Augmented/BlurAll")"""
+    sets = ["Other/", "YouTube/", "Meter_1/", "Meter_2/"]
     for s in sets:
         names = GetAllNames("../Images/Original/" + s)
         for name in names:
             im = cv2.imread("../Images/Original/" + s + name, 1)
+            im = cv2.resize(im, (32, 48))
             im = preproc(im)
-            cv2.imwrite("../Images/BlurAndThreshold/" + s + name, im)"""
+            cv2.imwrite("../Images/BlurAndThreshold/" + s + name, im)
 
 if __name__ == "__main__":
     main()
